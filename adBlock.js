d95615f2a64ed95eff79cdf130ab6c040818a51f
Mittiii <srp5628comust@gmail.com>
2023-08-11 23:08:01 +0900

    0.0.1 jnkn

--- adBlock.js
+++ adBlock.js
@@ -4,7 +4,8 @@ $(document).ready(() => { //ページ読み込み直後
     setTimeout(adBlock, 5000); //5秒後
     setTimeout(adBlock, 7000);*/
     for (let i = 0; i < 4; i++){
-        adBlock();
+        //adBlock();
+        setTimeout(adBlock, 2000)
       }
 });
 /*window.onload = () => { //ページ読み込み完了時

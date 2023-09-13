7ca39cbc2f8242454670e8f59f54fba43b2bc2c5
Mittiii <srp5628comust@gmail.com>
2023-09-14 04:42:42 +0900

    アイコン作成,削除する要素追加

--- adBlock.js
+++ adBlock.js
@@ -35,4 +35,7 @@ const adBlock = () => {
     removeElementsBySelector(".Introduction__item__image");
     removeElementsBySelector(".gn_sw-slider");
     removeElementsBySelector(".gn_rect_float_closed_icon");
+    removeElementsBySelector(".gn_delivery_1425972_8708dbd7");
+    removeElementsBySelector(".gn_rect_float_closed_icon");
+    removeElementsBySelector(".gn_delivery_1425972_ff00b595");
 };
--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => { //読み込み直後処理
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => { //今開いているタブ
         chrome.tabs.sendMessage(tabs[0].id, {}, (res) => { //メッセージを送る
-            setTimeout( () => { window.close(); }, 700); //0.7秒後にpopup.htmlを閉じる
+            setTimeout( () => { window.close(); }, 10000); //0.7秒後にpopup.htmlを閉じる
         });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var showButton = document.getElementById("submit_button");
+    var userInput = document.getElementById('user_input');
+
+    showButton.addEventListener('click', function () {
+        var text = userInput.value;
+        alert("このクラスを持つ要素をブロックします"+ text);
     });
 });

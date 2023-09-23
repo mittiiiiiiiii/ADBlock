57cce713d72d683d5a2410c5990ec02da1f59750
Mittiii <srp5628comust@gmail.com>
2023-09-24 00:12:00 +0900

    入力処理追加

--- main.js
+++ main.js
@@ -6,12 +6,33 @@ document.addEventListener("DOMContentLoaded", () => { //読み込み直後処理
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var showButton = document.getElementById("submit_button");
-    var userInput = document.getElementById('user_input');
 
-    showButton.addEventListener('click', function () {
-        var text = userInput.value;
-        alert("このクラスを持つ要素をブロックします"+ text);
+
+// ボタンクリック時の処理を定義
+document.getElementById("submit_button").addEventListener("click", function() {
+    const inputText = document.getElementById("user_input").value;
+    
+    // テキストをChromeストレージに保存
+    chrome.storage.local.set({ "savedText": inputText }, function() {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+        } else {
+            console.log("Text saved successfully!");
+        }
     });
+    alert("このクラスを持つ要素をブロックします"+ inputText);
 });
+
+
+// 入力値を保存する変数
+
+
+// 参照ボタンがクリックされたときに保存された入力値を表示する関数
+function referenceUserInput() {
+    const resultDiv = document.getElementById('result');
+    if (savedInputValue !== "") {
+        resultDiv.textContent = '保存された値: ' + savedInputValue;
+    } else {
+        resultDiv.textContent = '保存された値はありません。';
+    }
+}
